7027577a206933561f91d519637e6939d9fa271c
Hydrop002 <872835920@qq.com>
2023-06-23 17:24:32 +0800

    修改洞穴tab提示

--- src/main/java/org/utm/featurehelper/command/CommandCave.java
+++ src/main/java/org/utm/featurehelper/command/CommandCave.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import org.utm.featurehelper.feature.patch.CavesPatcher;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -74,24 +75,20 @@ public class CommandCave extends CommandBase {
                 float yaw = rand.nextFloat() * (float) Math.PI * 2F;
                 float pitch = (rand.nextFloat() - 0.5F) / 4F;
                 float radius = rand.nextFloat() * 2F + rand.nextFloat();
+                if (rand.nextInt(10) == 0)
+                    radius *= rand.nextFloat() * rand.nextFloat() * 3 + 1;
                 int length = 0;
                 boolean debug = false;
                 if (args.length >= 6) {
-                    if (!args[5].equals("*")) {  // useDefault
-                        yaw = sender instanceof EntityPlayer ? ((EntityPlayer) sender).rotationYaw : 0;
-                        yaw = (float) func_110666_a(sender, yaw, args[5]);
-                        yaw = (yaw + 90F) * (float) Math.PI / 180F;
-                    }
+                    yaw = sender instanceof EntityPlayer ? ((EntityPlayer) sender).rotationYaw : 0;
+                    yaw = (float) func_110666_a(sender, yaw, args[5]);
+                    yaw = (yaw + 90F) * (float) Math.PI / 180F;
                     if (args.length >= 7) {
-                        if (!args[6].equals("*")) {
-                            pitch = sender instanceof EntityPlayer ? ((EntityPlayer) sender).rotationPitch : 0;
-                            pitch = (float) func_110666_a(sender, pitch, args[6]);
-                            pitch = -pitch * (float) Math.PI / 180F;
-                        }
+                        pitch = sender instanceof EntityPlayer ? ((EntityPlayer) sender).rotationPitch : 0;
+                        pitch = (float) func_110666_a(sender, pitch, args[6]);
+                        pitch = -pitch * (float) Math.PI / 180F;
                         if (args.length >= 8) {
-                            if (!args[7].equals("*")) {
-                                radius = (float) parseDouble(sender, args[7]);
-                            }
+                            radius = (float) parseDouble(sender, args[7]);
                             if (args.length >= 9) {
                                 length = parseInt(sender, args[8]);
                                 if (args.length >= 10) {
@@ -121,7 +118,7 @@ public class CommandCave extends CommandBase {
             }
 
         } else {
-            throw new WrongUsageException("commands.cave.continue.usage");
+            throw new WrongUsageException("commands.cave.usage");
         }
     }
 
@@ -129,23 +126,52 @@ public class CommandCave extends CommandBase {
     public List addTabCompletionOptions(ICommandSender sender, String[] args) {
         if (args.length == 1)
             return getListOfStringsMatchingLastWord(args, new String[] {"start", "continue"});
-        else if (args.length == 5)
-            return getListOfStringsMatchingLastWord(args, new String[] {"tunnel", "room"});
-        else if (args.length == 10)
+        else if (args.length == 5) {
+            List list = getListOfStringsMatchingLastWord(args, new String[]{"tunnel", "room"});
+            if (list.contains("tunnel"))
+                func_152373_a(sender, this, "commands.cave.start.tunnel.usage");
+            if (list.contains("room"))
+                func_152373_a(sender, this, "commands.cave.start.room.usage");
+            return list;
+        } else if (args.length == 6) {
+            if (args[0].equals("start")) {
+                if (args[4].equals("room")) {
+                    Random rand = new Random();
+                    float radius = 1 + rand.nextFloat() * 6;
+                    return Arrays.asList(String.valueOf(radius));
+                } else if (args[4].equals("tunnel")) {
+                    Random rand = new Random();
+                    float yaw = rand.nextFloat() * 360;
+                    return Arrays.asList(String.valueOf(yaw));
+                }
+            }
+            return null;
+        } else if (args.length == 7) {
+            if (args[0].equals("start")) {
+                if (args[4].equals("tunnel")) {
+                    Random rand = new Random();
+                    float pitch = (rand.nextFloat() - 0.5F) * 45F / (float) Math.PI;
+                    return Arrays.asList(String.valueOf(pitch));
+                }
+            }
+            return null;
+        } else if (args.length == 8) {
+            if (args[0].equals("start")) {
+                if (args[4].equals("tunnel")) {
+                    Random rand = new Random();
+                    float radius = rand.nextFloat() * 2F + rand.nextFloat();
+                    if (rand.nextInt(10) == 0)
+                        radius *= rand.nextFloat() * rand.nextFloat() * 3 + 1;
+                    return Arrays.asList(String.valueOf(radius));
+                }
+            }
+            return null;
+        } else if (args.length == 10)
             if (args[0].equals("start") && args[4].equals("tunnel"))
                 return getListOfStringsMatchingLastWord(args, new String[] {"false", "true"});
             else
                 return null;
-        else if (args.length >= 6) {
-            if (args[0].equals("start")) {
-                if (args[4].equals("room")) {
-                    func_152373_a(sender, this, "commands.cave.start.room.usage");
-                } else if (args[4].equals("tunnel")) {
-                    func_152373_a(sender, this, "commands.cave.start.tunnel.usage");
-                }
-            }
-            return null;
-        } else
+        else
             return null;
     }
 
